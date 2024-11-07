1b10586069b27e5c4183f3e2c87b2084715e4b76
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-08 00:14:40 +0100

    updated tests

--- test/test.py
+++ test/test.py
@@ -1,27 +1,34 @@
+import importlib.util
 import pytest
 from config2class.entrypoint import Config2Code
+from config2class.utils.replacement import replace_tokens
 from test import OUT_PATH
 from test.fixture import cleanup
 from config2class.utils import deconstruct_config
 from config2class.utils import filesystem
+import importlib
 
 @pytest.mark.parametrize(
-    "file_name", ["example.json", "example.yaml", "example.toml", "example_flat.json", "example_flat.yaml", "example_flat.toml"]
+    "file_name", ["example.json", "example.yaml", "example.toml", "example_flat.json", "example_flat.yaml", "example_flat.toml", "example_token.yaml"]
 )
 def test_class_construction(cleanup, file_name: str):
     process = Config2Code()
     input_file = "example/" + file_name
     process.to_code(input_file, OUT_PATH)
+    
 
     # test from file
-    import test.tmp as tmp
+    spec = importlib.util.spec_from_file_location("tmp", "test/tmp.py")
+    generated_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(generated_module)
     try:
-        config = getattr(tmp, "App_config").from_file(input_file)
+        config = getattr(generated_module, "App_config").from_file(input_file)
     except AttributeError:
-        config = getattr(tmp, "Config").from_file(input_file)
+        config = getattr(generated_module, "Config").from_file(input_file)
 
     ending = file_name.split(".")[-1]
     config_file = getattr(filesystem, f"load_{ending}")(input_file)
+    config_file = replace_tokens(config_file)
     first_key, first_value = config_file.popitem()
     if len(config_file) == 0 and isinstance(first_value, dict):
         config_file = first_value
