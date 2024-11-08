4a7138602ebd961c1efb187ddc8f448fd920c0a0
Robin Uhrich <robin.uhrich@gmail.com>
2024-11-08 01:17:12 +0100

    added new test_services

--- test/test.py
+++ test/test.py
@@ -1,26 +1,35 @@
+import importlib
 import importlib.util
-import pytest
-from config2class.entrypoint import Config2Code
-from config2class.utils.replacement import replace_tokens
+import os
+import shutil
+import subprocess
+import time
 from test import OUT_PATH
 from test.fixture import cleanup
-from config2class.utils import deconstruct_config
-from config2class.utils import filesystem
-import importlib
-import os
+from test.utils import _check_created_config
+
+import pytest
+
+import config2class.service.api_funcs as api_funcs
+from config2class.entrypoint import Config2Code
+from config2class.service.api_funcs import start_observer, stop_process
+from config2class.utils import deconstruct_config, filesystem
+from config2class.utils.replacement import replace_tokens
+
+CONFIG_FILES = [
+    "example.json",
+    "example.yaml",
+    "example.toml",
+    "example_flat.json",
+    "example_flat.yaml",
+    "example_flat.toml",
+    "example_token.yaml",
+]
 
 
 @pytest.mark.parametrize(
     "file_name",
-    [
-        "example.json",
-        "example.yaml",
-        "example.toml",
-        "example_flat.json",
-        "example_flat.yaml",
-        "example_flat.toml",
-        "example_token.yaml",
-    ],
+    CONFIG_FILES,
 )
 def test_class_construction(cleanup, file_name: str):
     process = Config2Code()
@@ -28,23 +37,7 @@ def test_class_construction(cleanup, file_name: str):
     process.to_code(input_file, OUT_PATH)
 
     # test from file
-    spec = importlib.util.spec_from_file_location("tmp", "test/tmp.py")
-    generated_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(generated_module)
-    try:
-        config = getattr(generated_module, "App_config").from_file(input_file)
-    except AttributeError:
-        config = getattr(generated_module, "Config").from_file(input_file)
-
-    ending = file_name.split(".")[-1]
-    config_file = getattr(filesystem, f"load_{ending}")(input_file)
-    config_file = replace_tokens(config_file)
-    first_key, first_value = config_file.popitem()
-    if len(config_file) == 0 and isinstance(first_value, dict):
-        config_file = first_value
-    else:
-        config_file[first_key] = first_value
-    assert deconstruct_config(config) == config_file
+    _check_created_config(input_file)
 
 
 def test_unknown_file(cleanup):
@@ -58,9 +51,20 @@ def test_unknown_file(cleanup):
         assert True
 
 
-def test_service(cleanup, input_file):
+@pytest.mark.parametrize(
+    "file_name",
+    CONFIG_FILES,
+)
+def test_service(cleanup, file_name):
     # create file
-    with open(f"test/input_file", "w", encoding="utf-8") as file:
+    input_file = f"test/{file_name}"
+    with open(input_file, "w", encoding="utf-8") as file:
         file.writelines([])
 
+    thread = start_observer(input_file, OUT_PATH)
+    shutil.copyfile(f"example/{file_name}", input_file)
+
+    stop_process(thread.ident)
     
+    _check_created_config(input_file)
+    os.remove(input_file)   
---
+++ test/utils.py
@@ -0,0 +1,29 @@
+    
+import importlib
+from config2class.utils import filesystem
+from config2class.utils.deconstruction import deconstruct_config
+from config2class.utils.replacement import replace_tokens
+from test import OUT_PATH
+
+def _check_created_config(input_file: str):
+    spec = importlib.util.spec_from_file_location("tmp", OUT_PATH)
+    generated_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(generated_module)
+    print(generated_module.__dict__)
+    try:
+        config_cls = getattr(generated_module, "App_config")
+        config = config_cls.from_file(input_file)
+    except AttributeError:
+        config_cls = getattr(generated_module, "Config")
+        config = config_cls.from_file(input_file)
+
+    file_name = input_file.split("/")[-1]
+    ending = file_name.split(".")[-1]
+    config_file = getattr(filesystem, f"load_{ending}")(input_file)
+    config_file = replace_tokens(config_file)
+    first_key, first_value = config_file.popitem()
+    if len(config_file) == 0 and isinstance(first_value, dict):
+        config_file = first_value
+    else:
+        config_file[first_key] = first_value
+    assert deconstruct_config(config) == config_file
